5bd90fc18d5da3db042a70088e4c9df7380bb6db
r.zach.lamberty <r.zach.lamberty@gmail.com>
2016-06-26 09:34:57 -0400

    scrape: dead-simple wrapper script

--- scrape.py
+++ scrape.py
@@ -15,10 +15,10 @@ Usage:
 """
 
 import argparse
-import logging
-import logging.config
 import os
 import yaml
+
+import eri.logging as logging
 
 import cards
 import common
@@ -30,9 +30,63 @@ import scgdecks
 # ----------------------------- #
 
 # logging and file IO constants
-HERE = os.path.dirname(os.path.realpath(__file__))
 logging.getLogger("requests").setLevel(logging.WARNING)
 logger = logging.getLogger("scrape")
-LOGCONF = os.path.join(HERE, 'logging.yaml')
-with open(LOGCONF, 'rb') as f:
-    logging.config.dictConfig(yaml.load(f))
+logging.configure()
+
+
+# ----------------------------- #
+#   Main routine                #
+# ----------------------------- #
+
+def main(decksrc='scg', neo4jurl=common.NEO4J_URL):
+    """do ery ol thang
+
+    args:
+        decksrc: string, the source of deck database elements we should parse
+            into json for loading into our neo4j database. currently, only
+            "scg" is a supported option (default: scg)
+        neo4jurl: string, the url of the neo4j database into which we will load
+            card and deck info (default: common.NEO4J_URL)
+
+    returns: Nothing
+
+    raises:
+        ValueError: unhandled decksrc value
+
+    """
+    cards.json_to_neo4j()
+    if decksrc =='scg':
+        scgdecks.load_decks_to_neo4j()
+    else:
+        raise ValueError(
+            "decksrc '{}' is not a valid deck src enumeration".format(decksrc)
+        )
+
+
+# ----------------------------- #
+#   Command line                #
+# ----------------------------- #
+
+def parse_args():
+    """ Take a log file from the commmand line """
+    parser = argparse.ArgumentParser()
+
+    decksrc = "type of deck source we are using"
+    parser.add_argument("-d", "--decksrc", help=decksrc, default='scg')
+
+    neo4jurl = "url of the neo4j database"
+    parser.add_argument(
+        "-u", "--neo4jurl", help=neo4jurl, default=common.NEO4J_URL
+    )
+
+    args = parser.parse_args()
+
+    logger.debug("arguments set to {}".format(vars(args)))
+
+    return args
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    main()
